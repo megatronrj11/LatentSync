6e9583be77cd0a4d039ad4c53e546c80c2cb9efd
megatronrj11 <132009004+megatronrj11@users.noreply.github.com>
2025-02-08 23:32:35 +0530

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# Create a new conda environment
+Create a new conda environment
 conda create -y -n latentsync python=3.10.13
 conda activate latentsync
 
-# Install ffmpeg
+Install ffmpeg
 conda install -y -c conda-forge ffmpeg
 
-# Python dependencies
+Python dependencies
 pip install -r requirements.txt
 
-# OpenCV dependencies
+OpenCV dependencies
 sudo apt -y install libgl1
 
-# Download all the checkpoints from HuggingFace
-huggingface-cli download ByteDance/LatentSync --local-dir checkpoints --exclude "*.git*" "README.md"
 
-# Soft links for the auxiliary models
+
+Download all the checkpoints from HuggingFace
+huggingface-cli download ByteDance/LatentSync --local-dir checkpoints --exclude ".git" "README.md"
+
+Soft links for the auxiliary models
 mkdir -p ~/.cache/torch/hub/checkpoints
 ln -s $(pwd)/checkpoints/auxiliary/2DFAN4-cd938726ad.zip ~/.cache/torch/hub/checkpoints/2DFAN4-cd938726ad.zip
 ln -s $(pwd)/checkpoints/auxiliary/s3fd-619a316812.pth ~/.cache/torch/hub/checkpoints/s3fd-619a316812.pth
